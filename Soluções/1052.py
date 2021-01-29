a88df7b01cbf3b8f0e41c73bbf48cc7c8e3534da
Carlos Silva <carloscksilva@gmail.com>
2021-01-29 19:16:22 -0200

    Mês

---
+++ Soluções/1052.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+Exemplos de Entrada
+4
+
+Exemplos de Saída
+April
+"""
+meses = [
+	'January', 'February', 'March',
+	'April', 'May', 'June',
+	'July', 'August', 'September',
+	'October', 'November', 'December'
+]
+mes = int(input())
+print(meses[mes-1])
