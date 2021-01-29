8a9e89974c424decfd7dd459462bd54b906d67ab
Carlos Silva <carloscksilva@gmail.com>
2021-01-29 15:37:04 -0200

    Média 1

---
+++ Soluções/1005.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+Exemplos de Entrada
+5.0
+7.1
+
+Exemplos de Saída
+MEDIA = 6.43182
+"""
+
+A = float(input())
+B = float(input())
+
+M = (((A * 3.5) + (B * 7.5)) / 11)
+
+print(f'MEDIA = {M:.5f}')
