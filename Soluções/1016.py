eaf6c812d5405daa1995ca7175f0d5295618cea6
Carlos Silva <carloscksilva@gmail.com>
2021-01-29 18:19:45 -0200

    Distância

---
+++ Soluções/1016.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+Exemplos de Entrada
+30
+
+Exemplos de Saída
+60 minutos
+"""
+
+
+km = int(input())
+
+tempo = km * 2
+
+print(f'{tempo} minutos')
+
+"""
+Carro X = 60km/h << 30 km atras do carro Y
+Carro Y = 90km/h >> 30 km na frente do carro X >> distância de  1 km a cada 2 minuto...
+"""
