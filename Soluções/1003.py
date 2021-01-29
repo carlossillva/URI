4a705a7808fad61cb1c5f66012676bc1b9776712
Carlos Silva <carloscksilva@gmail.com>
2021-01-29 15:27:21 -0200

    Soma Simples

---
+++ Soluções/1003.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Exemplos de Entrada
+30
+10
+
+Exemplos de Saída
+SOMA = 40
+"""
+
+def soma(a, b):
+	return a + b
+
+
+a = int(input())
+b = int(input())
+
+soma = soma(a, b)
+print(f'SOMA = {soma}')
